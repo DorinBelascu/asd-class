9a6f61a1cf529bb063fb30ef042e9c320fa99ebd
calin.verdes <calin.verdes@comptech.ro>
2014-12-04 21:40:59 +0200

    catalog responsive

--- app/views/catalog.blade.php
+++ app/views/catalog.blade.php
@@ -6,53 +6,29 @@
   <div class="panel-heading">Selecteaza un elev din catalog</div>
   <div class="panel-body">
 
-  	<div>
+  	<div class="row">
   	@foreach($elevi as $i => $elev)
-	  @if ($i % 4)
-	  	<div class="col-md-3">
-		        <div style="width: 240px; text-align:center; margin:0px auto">
 
+	  	<div class="col-md-3 col-xs-6">
+		        <div style="text-align:center; margin:4px auto">
 		        	<a href="#">
-		        	<div class="elev">
-		        		<div class="afisare">
-		          			{{ HTML::image('images/photos/elevi/' . $elev->photo, $elev->photo,['class' => 'img-responsive']) }}   
-		          			<div class="sablon">
-		          				<div class="nume_elev">                   
-		          					{{ $elev->nume . ' ' . $elev->prenume }} 
-		          				</div>        
-		          			</div>     
-		          		</div> 
-		          	</div>
+			        	<div class="elev">
+			        		<div class="afisare">
+				          			{{ HTML::image('images/photos/elevi/' . $elev->photo, $elev->photo,['class' => 'img-responsive', 'style' => 'width:100%']) }} 
+				          			<div class="sablon">
+				          				<div class="nume_elev">                   
+				          					{{ $elev->nume . ' ' . $elev->prenume }} 
+				          				</div>        
+				          			</div>    
+				          			
+			          		</div> 
+			          	</div>
 		          	</a>
-
 		        </div>
 		</div>
-	  @else
-	  	</div>
-	  	<div class="row">
-	  		<div class="col-md-3">
-		        <div style="width: 240px; text-align:center; margin:0px auto">
-
-		        	<a href="#">
-		        	<div class="elev">
-		        		<div class="afisare">
-		          	{{ HTML::image('images/photos/elevi/' . $elev->photo, $elev->photo,['class' => 'img-responsive']) }}   
-		          			<div class="sablon">
-		          				<div class="nume_elev">                   
-		          					{{ $elev->nume . ' ' . $elev->prenume }} 
-		          				</div>     
-		          			</div>     
-		          		</div> 
-		          	</div>
-		         	</a>
-
-		        </div>
-			</div>
-
-	  @endif
+	  
 	@endforeach
 	</div>
-
   </div>
 </div>
 
