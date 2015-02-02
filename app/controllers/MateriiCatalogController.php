c7a7d6e9fee6f22161e4612d81ab48b67f164771
Dorin Belascu <Dorin.Belascu@yahoo.com>
2015-02-02 18:57:42 +0200

    catalog medii

--- app/controllers/MateriiCatalogController.php
+++ app/controllers/MateriiCatalogController.php
@@ -6,6 +6,65 @@ class MateriiCatalogController extends BaseController
 	public function index($id)
 	{
 		$elev = Elevi::find($id);
+		$note = Note::where('elev_id', '=', $id)->get();
+		$k1 = 0; $k2 = 0; $s1 = 0; $s2 = 0; $teza1 = 0; $teza2 = 0;
+		foreach ($note as $i => $nota) {
+			if($nota->semestru==1)
+			{
+				if ($nota->teza) 
+				{
+					$teza1 = $nota->valoare;
+				}
+				else
+				{
+					$s1 += $nota->valoare;
+					$k1 += 1;	
+				}
+			}
+			else
+			{
+				if ($nota->teza) 
+				{
+					$teza2 = $nota->valoare;
+				}
+				$s2 += $nota->valoare;
+				$k2 += 1;
+			}
+		}
+		$media1 = 'Elevul nu are note la aceasta materie'; 
+		$media2 = 'Elevul nu are note la aceasta materie';
+		if($k1)
+		{
+			if ($teza1) 
+			{
+				$media1 = round(($s1/$k1*3+$teza1)/4,2);
+			}
+			else
+			{
+				$media1 = round($s1/$k1,2);
+			}
+		}
+		if($k2)
+		{
+			if ($teza2) 
+			{
+				$media2 = round(($s2/$k2*3+$teza2)/4,2);
+			}
+			else
+			{
+				$media2 = round($s2/$k2,2);
+			}
+		}
+
+    	if(($media1<>'Elevul nu are note la aceasta materie') || ($media2<>'Elevul nu are note la aceasta materie')) 
+    	{
+    		$mediatot = round(($media1+$media2)/2,2);
+    	}
+    	else
+    	{
+    		$mediatot = $media1+$media2;	
+    	}
+
 		if(! $elev)
 		{
 			return Redirect::route('catalog');
@@ -13,7 +72,10 @@ class MateriiCatalogController extends BaseController
 	 	$materii = Materii::orderBy('denumirea')->get();
 	 	return View::make('materii-catalog')->with([
 	 		'materii' => $materii, 
-	 		'id'      => $id
+	 		'id'      => $id,
+	 		'media1'  => $media1,
+	 		'media2'  => $media2,
+	 		'mediatot'=> $mediatot
 	 	]);
 	}
 }
--- app/views/materii-catalog.blade.php
+++ app/views/materii-catalog.blade.php
@@ -6,31 +6,97 @@
   <div class="panel-heading">Selecteaza o materie</div>
   <div class="panel-body">
   	<div class="row">
+
   	@foreach($materii as $i => $materie)
-	  	<div class="col-md-3 col-xs-6">
+
+<div class="col-md-3 col-xs-6">
+<div style="text-align:center; margin:4px auto">
+	<div class="dropdown" style="position:relative">
+	   	<a href="#" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">{{ $materie->denumirea}} <span class="caret"></span></a>
+		<ul class="dropdown-menu">
+			<li>
+		    	{{ HTML::link(URL::route('catalog-note',[
+		    	'denumirea'  => $materie->denumirea, 
+		    	'id_elev'    => $id,
+		    	'id_materie' => $materie->id]), 'Note ...') }}
+			</li>
+		    <li>
+		    	{{ HTML::link(URL::route('catalog-absente',[
+		    	'denumirea'  => $materie->denumirea, 
+		    	'id_elev'    => $id,
+		    	'id_materie' => $materie->id]), 'Absente ...') }}
+		    </li>
+			<li>
+				<a class="trigger right-caret">Media</a>
+				<ul class="dropdown-menu sub-menu">
+					<li><a href="#">Media pe semestrul 1 : {{$media1}}</a></li>
+					<li><a href="#">Media pe semestrul 2 : {{$media2}}</a></li>
+					<li><a href="#">Media pe ambele semestre : {{$mediatot}}</a></li>
+				</ul>
+			</li>
+		</ul>
+	</div>
+</div>
+</div>
+
+<!-- 	  	<div class="col-md-3 col-xs-6">
 		        <div style="text-align:center; margin:4px auto">
 	          		<div class="btn-group">
 					  <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
 					    {{ $materie->denumirea}} <span class="caret"></span>
 					  </button>
-					  <ul class="dropdown-menu" role="menu">
-					    <li>{{ HTML::link(URL::route('catalog-note',[
+					  <ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
+					    <li>
+					    	{{ HTML::link(URL::route('catalog-note',[
 					    	'denumirea'  => $materie->denumirea, 
 					    	'id_elev'    => $id,
-					    	'id_materie' => $materie->id]), 'Note ...') }}</li>
+					    	'id_materie' => $materie->id]), 'Note ...') }}
+					    </li>
 					    <li class="divider"></li>
-					    <li>{{ HTML::link(URL::route('catalog-absente',[
+					    <li>
+					    	{{ HTML::link(URL::route('catalog-absente',[
 					    	'denumirea'  => $materie->denumirea, 
 					    	'id_elev'    => $id,
-					    	'id_materie' => $materie->id]), 'Absente ...') }}</li>
-					    <li></li>
+					    	'id_materie' => $materie->id]), 'Absente ...') }}
+					    </li>
+
+					    <li class="divider"></li>
+						<li>
+						</li>
 					  </ul>
 					</div>
 		        </div>
-		</div>
+		</div> -->
 	@endforeach
 	</div>
   </div>
 </div>
 
+
+
+
+
+
+@stop
+
+@section('js')
+<script>
+$(function(){
+	$(".dropdown-menu > li > a.trigger").on("click",function(e){
+		var current=$(this).next();
+		var grandparent=$(this).parent().parent();
+		if($(this).hasClass('left-caret')||$(this).hasClass('right-caret'))
+			$(this).toggleClass('right-caret left-caret');
+		grandparent.find('.left-caret').not(this).toggleClass('right-caret left-caret');
+		grandparent.find(".sub-menu:visible").not(current).hide();
+		current.toggle();
+		e.stopPropagation();
+	});
+	$(".dropdown-menu > li > a:not(.trigger)").on("click",function(){
+		var root=$(this).closest('.dropdown');
+		root.find('.left-caret').toggleClass('right-caret left-caret');
+		root.find('.sub-menu:visible').hide();
+	});
+});
+</script>
 @stop
